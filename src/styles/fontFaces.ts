f2d319e9502ceee3323465d8650674581b541e7f
David Cech <lilcech@gmail.com>
2020-01-21 16:08:24 +0100

    fix: fonts not loading properly

--- src/styles/fontFaces.ts
+++ src/styles/fontFaces.ts
@@ -3,26 +3,26 @@ import { css } from 'styled-components'
 export default css`
   @font-face {
     font-family: 'Helvetica Neue';
-    src: url("../../static/fonts/HelveticaNeue.ttf");
-    font-weight: 400;
+    src: url("/fonts/HelveticaNeue Light.ttf");
+    font-weight: 200;
     font-style: normal;
   }
   @font-face {
     font-family: 'Helvetica Neue';
-    src: url("../../static/fonts/HelveticaNeue Medium.ttf");
-    font-weight: 500;
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'Helvetica Neue';
-    src: url("../../static/fonts/HelveticaNeue Light.ttf");
+    src: url("/fonts/HelveticaNeue.ttf");
     font-weight: 300;
     font-style: normal;
   }
   @font-face {
     font-family: 'Helvetica Neue';
-    src: url("../../static/fonts/HelveticaNeue Bold.ttf");
-    font-weight: 600;
+    src: url("/fonts/HelveticaNeue Medium.ttf");
+    font-weight: 400;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Helvetica Neue';
+    src: url("/fonts/HelveticaNeue Bold.ttf");
+    font-weight: 500;
     font-style: normal;
   }
 `
